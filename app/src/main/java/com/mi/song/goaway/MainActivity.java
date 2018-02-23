d7901d5d918305a48ed6e95e0fbad8c45ada0dde
songhang <songhang@xiaomi.com>
2018-02-23 14:19:32 +0800

    添加打赏功能

--- app/src/main/java/com/mi/song/goaway/MainActivity.java
+++ app/src/main/java/com/mi/song/goaway/MainActivity.java
@@ -1,15 +1,31 @@
 package com.mi.song.goaway;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.didikee.donate.AlipayDonate;
+import android.didikee.donate.WeiXinDonate;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 2323;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +41,24 @@ public class MainActivity extends AppCompatActivity {
                 return super.shouldOverrideUrlLoading(view, url);
             }
         });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                wechatPay();
+            } else {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void use(View view) {
         try {
             Intent intent = new Intent();
-            intent.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);//android.service.wallpaper.CHANGE_LIVE_WALLPAPER
+            intent.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
             intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT
                     , new ComponentName(getApplicationContext().getPackageName()
                             , GoAwayWallpaperService.class.getCanonicalName()));
@@ -43,6 +70,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pay(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setItems(new String[]{"支付宝", "微信"}, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    aliPay();
+                } else {
+                    wechatPay();
+                }
+            }
+        });
+        builder.create().show();
+    }
 
+    private void wechatPay() {
+        //检测微信是否安装
+        if (!WeiXinDonate.hasInstalledWeiXinClient(this)) {
+            Toast.makeText(this, "未安装微信客户端", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //已经有权限
+            new AlertDialog.Builder(this)
+                    .setTitle("微信捐赠操作步骤")
+                    .setMessage("点击确定按钮后会跳转微信扫描二维码界面：\n\n" + "1. 点击右上角的菜单按钮\n\n" + "2. 点击'从相册选取二维码'\n\n" + "3. 选择第一张二维码图片即可\n\n")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            InputStream weixinQrIs = getResources().openRawResource(R.raw.didikee_weixin);
+                            String qrPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "AndroidDonateSample" + File.separator +
+                                    "didikee_weixin.png";
+                            WeiXinDonate.saveDonateQrImage2SDCard(qrPath, BitmapFactory.decodeStream(weixinQrIs));
+                            WeiXinDonate.donateViaWeiXin(MainActivity.this, qrPath);
+                        }
+                    })
+                    .setNegativeButton("取消", null)
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }
+    }
+
+    private void aliPay() {
+        boolean hasInstalledAlipayClient = AlipayDonate.hasInstalledAlipayClient(this);
+        if (hasInstalledAlipayClient) {
+            AlipayDonate.startAlipayClient(this, "FKX08327O1EEEDGRVIWIFB");
+        } else {
+            Toast.makeText(this, "未安装支付宝客户端", Toast.LENGTH_SHORT).show();
+        }
     }
 }
