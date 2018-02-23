b58469cdeecbf127b37a23d930825307c5d3a57b
songhang <songhang@xiaomi.com>
2018-02-23 13:27:26 +0800

    添加说明

--- app/src/main/java/com/mi/song/goaway/MainActivity.java
+++ app/src/main/java/com/mi/song/goaway/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("https://github.com/songhanghang/study_note/blob/master/%E8%BF%9C%E7%A6%BB%E6%89%8B%E6%9C%BA.md");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
 
+    }
+
+    public void use(View view) {
         try {
             Intent intent = new Intent();
             intent.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);//android.service.wallpaper.CHANGE_LIVE_WALLPAPER
@@ -26,5 +40,9 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void pay(View view) {
+
     }
 }
