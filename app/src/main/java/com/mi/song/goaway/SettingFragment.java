6d004d74ce98f004c63186f64bf15a0006e57169
LeeReindeer <reindeerlee.work@gmail.com>
2018-03-03 22:56:49 +0800

    Add summary for bottom distance

--- app/src/main/java/com/mi/song/goaway/SettingFragment.java
+++ app/src/main/java/com/mi/song/goaway/SettingFragment.java
@@ -9,6 +9,8 @@ import android.support.annotation.Nullable;
 import com.takisoft.fix.support.v7.preference.EditTextPreference;
 import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
+import java.util.Locale;
+
 public class SettingFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     public static String PRE_KEY_TIP0 = "pre_key_tip0";
@@ -22,6 +24,8 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
     EditTextPreference editText2;
     EditTextPreference editText3;
 
+    EditTextPreference editTextBottom;
+
     public SettingFragment() {
         // Required empty public constructor
     }
@@ -34,6 +38,7 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
         editText1 = (EditTextPreference) findPreference(PRE_KEY_TIP1);
         editText2 = (EditTextPreference) findPreference(PRE_KEY_TIP2);
         editText3 = (EditTextPreference) findPreference(PRE_KEY_TIP3);
+        editTextBottom = (EditTextPreference) findPreference(PRE_KEY_BOTTOM);
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
         setSummary(sp);
@@ -41,11 +46,7 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
-        editText0.setSummary(sharedPreferences.getString(PRE_KEY_TIP0, getString(R.string.tip0)));
-        editText1.setSummary(sharedPreferences.getString(PRE_KEY_TIP1, getString(R.string.tip1)));
-        editText2.setSummary(sharedPreferences.getString(PRE_KEY_TIP2, getString(R.string.tip2)));
-        editText3.setSummary(sharedPreferences.getString(PRE_KEY_TIP3, getString(R.string.tip3)));
+        setSummary(sharedPreferences);
     }
 
     @Override
@@ -59,5 +60,19 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
         editText1.setSummary(sharedPreferences.getString(PRE_KEY_TIP1, getString(R.string.tip1)));
         editText2.setSummary(sharedPreferences.getString(PRE_KEY_TIP2, getString(R.string.tip2)));
         editText3.setSummary(sharedPreferences.getString(PRE_KEY_TIP3, getString(R.string.tip3)));
+
+        int height = ScreenUtil.getHeight(getContext());
+        String bottomInPreference = sharedPreferences.getString(PRE_KEY_BOTTOM, getString(R.string.bottom_distance));
+        int bottomDistance = Integer.valueOf(bottomInPreference);
+
+        //check range
+        if (bottomDistance < 0 || bottomDistance > height) {
+            bottomInPreference = getString(R.string.bottom_distance);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(PRE_KEY_BOTTOM, bottomInPreference);
+            editor.apply();
+        }
+
+        editTextBottom.setSummary(String.format(Locale.getDefault(), getString(R.string.bottom_summary_format), height, bottomDistance));
     }
 }
