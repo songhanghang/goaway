0beb9e26c92852edea9be04c4c18c55d7174a905
LeeReindeer <reindeerlee.work@gmail.com>
2018-03-03 22:24:45 +0800

    Auto summary EditTextPreference

--- app/src/main/java/com/mi/song/goaway/SettingFragment.java
+++ app/src/main/java/com/mi/song/goaway/SettingFragment.java
@@ -1,16 +1,25 @@
 package com.mi.song.goaway;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
+import com.takisoft.fix.support.v7.preference.EditTextPreference;
 import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingFragment extends PreferenceFragmentCompat {
+public class SettingFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     public static String PRE_KEY_TIP0 = "pre_key_tip0";
     public static String PRE_KEY_TIP1 = "pre_key_tip1";
     public static String PRE_KEY_TIP2 = "pre_key_tip2";
     public static String PRE_KEY_TIP3 = "pre_key_tip3";
+
+    EditTextPreference editText0;
+    EditTextPreference editText1;
+    EditTextPreference editText2;
+    EditTextPreference editText3;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -19,5 +28,35 @@ public class SettingFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference, rootKey);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        editText0 = (EditTextPreference) findPreference(PRE_KEY_TIP0);
+        editText1 = (EditTextPreference) findPreference(PRE_KEY_TIP1);
+        editText2 = (EditTextPreference) findPreference(PRE_KEY_TIP2);
+        editText3 = (EditTextPreference) findPreference(PRE_KEY_TIP3);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        setSummary(sp);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        editText0.setSummary(sharedPreferences.getString(PRE_KEY_TIP0, getString(R.string.tip0)));
+        editText1.setSummary(sharedPreferences.getString(PRE_KEY_TIP1, getString(R.string.tip1)));
+        editText2.setSummary(sharedPreferences.getString(PRE_KEY_TIP2, getString(R.string.tip2)));
+        editText3.setSummary(sharedPreferences.getString(PRE_KEY_TIP3, getString(R.string.tip3)));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setSummary(SharedPreferences sharedPreferences) {
+        editText0.setSummary(sharedPreferences.getString(PRE_KEY_TIP0, getString(R.string.tip0)));
+        editText1.setSummary(sharedPreferences.getString(PRE_KEY_TIP1, getString(R.string.tip1)));
+        editText2.setSummary(sharedPreferences.getString(PRE_KEY_TIP2, getString(R.string.tip2)));
+        editText3.setSummary(sharedPreferences.getString(PRE_KEY_TIP3, getString(R.string.tip3)));
     }
 }
